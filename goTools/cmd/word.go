b08153234855f53172ba937e589eba04bc748903
qixia1998 <qixia717138552@gmail.com>
2023-01-28 22:59:07 +0800

    updated word

--- goTools/cmd/word.go
+++ goTools/cmd/word.go
@@ -8,24 +8,23 @@ import (
 )
 
 const (
-	ModeUpper                      = iota + 1 // 全部单词转为大写
-	ModeLower                                 // 全部单词转为小写
-	ModeUnderscoreToUpperCamelCase            // 下划线单词转为大写驼峰单词
-	ModeUnderscoreToLowerCamelCase            // 下划线单词转为小写驼峰单词
-	ModeCamelCaseToUnderscore                 // 驼峰单词转为下划线单词
+	ModeUpper                      = iota + 1 // 全部转大写
+	ModeLower                                 // 全部转小写
+	ModeUnderscoreToUpperCamelCase            // 下划线转大写驼峰
+	ModeUnderscoreToLowerCamelCase            // 下线线转小写驼峰
+	ModeCamelCaseToUnderscore                 // 驼峰转下划线
 )
-
-var desc = strings.Join([]string{
-	"该命令支持各种单词格式转换，模式如下：",
-	"1: 全部单词转为大写",
-	"2: 全部单词转为小写",
-	"3: 下划线单词转为小写驼峰单词",
-	"4: 下划线单词转为小写驼峰单词",
-	"5: 驼峰单词转为下划线单词",
-}, "\n")
 
 var str string
 var mode int8
+var desc = strings.Join([]string{
+	"该子命令支持各种单词格式转换，模式如下：",
+	"1：全部转大写",
+	"2：全部转小写",
+	"3：下划线转大写驼峰",
+	"4：下划线转小写驼峰",
+	"5：驼峰转下划线",
+}, "\n")
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
@@ -45,8 +44,9 @@ var wordCmd = &cobra.Command{
 		case ModeCamelCaseToUnderscore:
 			content = word.CamelCaseToUnderscore(str)
 		default:
-			log.Fatalf("暂不支持该转换模式，请执行help word查看帮助文档")
+			log.Fatalf("暂不支持该转换模式，请执行 help word 查看帮助文档")
 		}
+
 		log.Printf("输出结果: %s", content)
 	},
 }
