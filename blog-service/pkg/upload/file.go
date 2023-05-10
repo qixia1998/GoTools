61aaa4f6fe67d6fbabc110a0579cbeaeb9c02421
qixia1998 <qixia717138552@gmail.com>
2023-05-10 21:50:56 +0800

    updated file

--- blog-service/pkg/upload/file.go
+++ blog-service/pkg/upload/file.go
@@ -3,6 +3,10 @@ package upload
 import (
 	"GoTools/blog-service/global"
 	"GoTools/blog-service/pkg/util"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
 	"path"
 	"strings"
 )
@@ -25,4 +29,66 @@ func GetFileExt(name string) string {
 
 func GetSavePath() string {
 	return global.AppSetting.UploadSavePath
+}
+
+func CheckSavePath(dst string) bool {
+	_, err := os.Stat(dst)
+	return os.IsNotExist(err)
+}
+
+func CheckContainExt(t FileType, name string) bool {
+	ext := GetFileExt(name)
+	ext = strings.ToUpper(ext)
+	switch t {
+	case TypeImage:
+		for _, allowExt := range global.AppSetting.UploadImageAllowExts {
+			if strings.ToUpper(allowExt) == ext {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func CheckMaxSize(t FileType, f multipart.File) bool {
+	content, _ := ioutil.ReadAll(f)
+	size := len(content)
+	switch t {
+	case TypeImage:
+		if size >= global.AppSetting.UploadImageMaxSize*1024*1024 {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckPermission(dst string) bool {
+	_, err := os.Stat(dst)
+	return os.IsPermission(err)
+}
+
+func CreateSavePath(dst string, perm os.FileMode) error {
+	err := os.MkdirAll(dst, perm)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SaveFile(file *multipart.FileHeader, dst string) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, src)
+	return err
 }
